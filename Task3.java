2b33e60d0c32bbcc7c8b38200903c380864d2a9b
Ilari <ilari.richardt@gmail.com>
2018-10-09 17:05:59 +0000

    WIP

--- Task3.java
+++ Task3.java
@@ -10,6 +10,10 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Vector;
+import java.util.regex.Pattern;
+import java.util.Collections;
+import java.util.Map;
+
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -22,7 +26,9 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.fs.FileSystem;
 
+
 public class Task3 {
+
 
     // http://commons.apache.org/proper/commons-text/jacoco/org.apache.commons.text.similarity/JaccardSimilarity.java.html
     public static Double calculateJaccardSimilarity(final CharSequence left, final CharSequence right) {
@@ -45,9 +51,57 @@ public class Task3 {
         return 1.0d * intersectionSize / unionSet.size();
     }
 
+
+    // https://github.com/tdebatty/java-string-similarity/blob/master/src/main/java/info/debatty/java/stringsimilarity/Jaccard.java
+    private static final Pattern SPACE_REG = Pattern.compile("\\s+");
+    private static final int k = 3;
+    public static final Map<String, Integer> getProfile(final String string) {
+        HashMap<String, Integer> shingles = new HashMap<String, Integer>();
+
+        String string_no_space = SPACE_REG.matcher(string).replaceAll(" ");
+        for (int i = 0; i < (string_no_space.length() - k + 1); i++) {
+            String shingle = string_no_space.substring(i, i + k);
+            Integer old = shingles.get(shingle);
+            if (old != null) {
+                shingles.put(shingle, old + 1);
+            } else {
+                shingles.put(shingle, 1);
+            }
+        }
+
+        return Collections.unmodifiableMap(shingles);
+    }
+
+    public static Double calculateJaccardSimilarityAlt(final String s1, final String s2) {
+        if (s1 == null) {
+            throw new NullPointerException("s1 must not be null");
+        }
+
+        if (s2 == null) {
+            throw new NullPointerException("s2 must not be null");
+        }
+
+        if (s1.equals(s2)) {
+            return 1.0;
+        }
+
+        Map<String, Integer> profile1 = getProfile(s1);
+        Map<String, Integer> profile2 = getProfile(s2);
+
+
+        Set<String> union = new HashSet<String>();
+        union.addAll(profile1.keySet());
+        union.addAll(profile2.keySet());
+
+        int inter = profile1.keySet().size() + profile2.keySet().size()
+                - union.size();
+
+        return 1.0 - (1.0 * inter / union.size());
+    }
+
     public static class PairGenerator extends Mapper<Object, Text, Text, DoubleWritable>{
 
-        private final static DoubleWritable one = new DoubleWritable(1);
+        private final static DoubleWritable v = new DoubleWritable(1);
         private static Vector<String> File2Map = new Vector<String>();
         private String word = new String();
         private BufferedReader brReader;
@@ -89,7 +143,10 @@ public class Task3 {
             while (itr.hasMoreTokens()) {
                 word = itr.nextToken().toLowerCase();
                 for(String s : File2Map){
-                    context.write(new Text(word+"||"+s), one);
+                    Double v = calculateJaccardSimilarityAlt(word,s);
+                    if(v>=0 && v <=0.15) {
+                        context.write(new Text(word + "||" + s), new DoubleWritable(v));
+                    }
                 }
             }
         }
@@ -112,8 +169,7 @@ public class Task3 {
         job.addCacheFile(p1.toUri());
 
         job.setMapperClass(PairGenerator.class);
-//        job.setCombinerClass(IntSumReducer.class);
-//        job.setReducerClass(IntSumReducer.class);
+
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(DoubleWritable.class);
         FileInputFormat.addInputPath(job, p0);
